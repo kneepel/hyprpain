44da344b199489fd27f32bbf6c650b9167d82fe7
kneepel <kneepel@outlook.com>
2024-09-14 21:35:11 -0700

    initial commit for packages

---
+++ files/scripts/rpmfusion.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+set -oue pipefail
+
+#install current rpmfusion repo
+rpm-ostree install \
+  https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm \
+  https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
