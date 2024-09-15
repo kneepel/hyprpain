19c032d571d02e2c2f94859d0c9bffe69d72f6c3
kneepel <kneepel@outlook.com>
2024-09-14 22:48:03 -0700

    a different approach

--- files/scripts/hyprland.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-set -oue pipefail
-
-#install hyprland (and other packages) without pulling defaults
-rpm-ostree install hyprland xdg-desktop-portal-hyprland waypaper swww hyprlock hypridle --recommends=false
